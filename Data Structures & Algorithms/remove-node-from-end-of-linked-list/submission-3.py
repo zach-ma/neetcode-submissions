cd29d3ea1568ea1a2472364bc1aec4306b24e59e
Zach-Ma <63674024+zach-ma@users.noreply.github.com>
2026-04-29 12:15:09 -0400

    Add: remove-node-from-end-of-linked-list - submission-3

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-3.py
@@ -0,0 +1,114 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        ''' REDO!!!
+        1. Brute Force
+        We store all nodes in an array so we can directly access the node that is n positions from the end.
+        Once we know which node to delete, we simply adjust the next pointer of the previous node.
+        T: O(n)
+        S: O(n)
+        '''
+        nodes = []
+        cur = head
+        while cur:
+            nodes.append(cur)
+            cur = cur.next
+        
+        removeIndex = len(nodes) - n # NOTE: use removeIndex = (N - n) to track!!!
+        if removeIndex == 0: # NOTE: boundary edge case!!!
+            return head.next
+
+        nodes[removeIndex - 1].next = nodes[removeIndex].next # NOTE: use [removeIndex].next not [removeIndex+1] to avoid index out of range!!!
+        return head
+
+        '''
+        2.0 Iteration (Two Pass), my failed soln
+        '''
+        # size = 0
+        # cur = head
+        # while cur:
+        #     size += 1
+        #     cur = cur.next
+        
+        # cur = head
+        # while size > n + 1:
+        #     size -= 1
+        #     cur = cur.next
+        # if cur and cur.next:
+        #     tmp = cur.next
+        #     cur.next = cur.next.next
+        #     tmp.next = None
+        
+        # return head
+
+        ''' REDO!!!
+        2. Iteration (Two Pass)
+        T: O(n)
+        S: O(1)
+        '''
+        # N = 0
+        # cur = head
+        # while cur:
+        #     N += 1
+        #     cur = cur.next
+        
+        # removeIndex = N - n # NOTE: use removeIndex = (N - n) to track!!!
+        # if removeIndex == 0:
+        #     return head.next
+        
+        # cur = head
+        # for i in range(N - 1):
+        #     if i + 1 == removeIndex: # NOTE: traverse again until reaching the node BEFORE removeIndex
+        #         cur.next = cur.next.next
+        #         break
+        #     cur = cur.next
+        # return head
+
+        ''' REDO????????????????????????
+        3. Recursion
+        '''
+
+        ''' REDO!!!!!!
+        4. Two Pointers + Dummy Node Pattern (iteration, one pass)
+        '''
+        dummy = Node(0, head)
+
+        # init
+        left = dummy
+        right = head
+        while n > 0 and right:
+            right = right.next
+            n -= 1
+        
+        # shift
+        while right:
+            left = left.next
+            right = right.next
+        
+        # delete
+        left.next = left.next.next
+
+        return dummy.next
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
