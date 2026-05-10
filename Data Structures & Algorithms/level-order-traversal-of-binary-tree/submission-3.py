4e079742c040723aa520b51a7c711ccc2a34b8d5
Zach-Ma <63674024+zach-ma@users.noreply.github.com>
2026-05-10 10:52:49 -0400

    Add: level-order-traversal-of-binary-tree - submission-3

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-3.py
@@ -0,0 +1,83 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        '''
+        1.0 BFS(my soln)
+        '''
+        # res = []
+        # if not root:
+        #     return res
+
+        # q = deque([root])
+        # while q:
+        #     curLevel = []
+        #     curSize = len(q)
+        #     for i in range(curSize):
+        #         node = q.popleft()
+        #         curLevel.append(node.val)
+
+        #         if node.left: # NOTE: redundant checking, can check if node instead
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+        #         if i == curSize - 1: # NOTE: can move out of for loop
+        #             res.append(curLevel)
+        # return res
+
+        ''' REDO!!!! 
+        1.1 BFS
+        '''
+        # res = []
+        # q = deque([root])
+        # while q:
+        #     qLen = len(q)
+        #     level = []
+        #     for i in range(qLen):
+        #         node = q.popleft()
+        #         if node:
+        #             level.append(node.val)
+        #             q.append(node.left)
+        #             q.append(node.right)
+        #     if level:
+        #         res.append(level)
+        # return res
+                    
+        ''' REDO!!!! can't solve!
+        2. DFS
+        '''
+        res = []
+        
+        def dfs(node, depth):
+            if not node:
+                return
+            if depth == len(res):
+                res.append([]) # NOTE: create empty container as you go!!!!
+            res[depth].append(node.val) # NOTE: append to corresponding container!!!
+            dfs(node.left, depth + 1)
+            dfs(node.right, depth + 1)
+        
+        dfs(root, 0)
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+            
+
