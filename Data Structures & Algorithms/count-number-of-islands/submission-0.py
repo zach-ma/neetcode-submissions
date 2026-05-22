193da465d5ff8978263493f490316c4c09cc55d9
Zach-Ma <63674024+zach-ma@users.noreply.github.com>
2026-05-22 10:28:32 -0400

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.py
@@ -0,0 +1,44 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        '''
+        1. Depth First Search
+        '''
+        res = 0
+        visited = set()
+
+        def dfs(r, c, cur):
+            if r < 0 or r >= len(grid):
+                return cur
+            if c < 0 or c >= len(grid[0]):
+                return cur
+            
+            if (r, c) in visited or grid[r][c] == '0':
+                return cur
+
+            elif grid[r][c] == '1':
+                grid[r][c] = '0'
+                cur = 1
+                return max(
+                    dfs(r-1, c, cur),
+                    dfs(r+1, c, cur),
+                    dfs(r, c-1, cur),
+                    dfs(r, c+1, cur)
+                )
+        
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if (r, c) not in visited:
+                    res += dfs(r, c, 0)
+
+        return res
+
+                
+            
+            
+        
+        '''
+        2. Breadth First Search
+        '''
+        '''
+        3. Disjoint Set Union
+        '''
