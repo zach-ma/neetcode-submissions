bbd662552d35644f505b1092e5a84d7883f66e6e
Zach-Ma <63674024+zach-ma@users.noreply.github.com>
2026-05-26 16:49:04 -0400

    Add: evaluate-reverse-polish-notation - submission-3

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-3.py
@@ -0,0 +1,38 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        # stack = []
+        # for t in tokens:
+        #     if t in {'+', '-', '*', '/'}:
+        #         b = stack.pop() # NOTE: order matters!!!
+        #         a = stack.pop()
+        #         if t == '+':
+        #             stack.append(a + b)
+        #         elif t == '-':
+        #             stack.append(a - b)
+        #         elif t == '*':
+        #             stack.append(a * b)
+        #         elif t == '/': # NOTE: can use int() instead of manually!!!
+        #             if (a > 0 and b > 0) or (a < 0 and b < 0):
+        #                 stack.append(abs(a) // abs(b))
+        #             else:
+        #                 stack.append(-(abs(a) // abs(b)))
+        #     else:
+        #         stack.append(int(t))
+        # return stack[0]
+
+        stack = []
+        for c in tokens:
+            if c == '+':
+                stack.append(stack.pop() + stack.pop())
+            elif c == '-':
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == '*':
+                stack.append(stack.pop() * stack.pop())
+            elif c == '/':
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b / a)) # use int() to round towards zero!!!!! 
+            else:
+                stack.append(int(c))
+        return stack[0]
+        
